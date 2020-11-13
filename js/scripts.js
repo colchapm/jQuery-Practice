0102c9c0eeb601175162404a6fc5f2a1836dd251
Collin Chapman <cchap14@gmail.com>
2020-11-12 19:04:03 -0800

    create colors.html page with feature to choose favorite color and change the background of the webpage to that color.

--- js/scripts.js
+++ js/scripts.js
@@ -4,3 +4,20 @@ $(document).ready(function() {
     $("#initially-hidden").toggle();
   });
 });
+
+$(document).ready(function() {
+  $("button#green").click(function() {
+    $("body").removeClass();
+    $("body").addClass("green-background");
+  });
+
+  $("button#yellow").click(function() {
+    $("body").removeClass();
+    $("body").addClass("yellow-background");
+  });
+
+  $("button#red").click(function() {
+    $("body").removeClass();
+    $("body").addClass("red-background");
+  });
+});
